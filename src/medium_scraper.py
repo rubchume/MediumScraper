c114e19017d04d9875abe490bc162f3188e28b71
Rubén Chuliá Mena <rubchume@gmail.com>
2021-04-22 22:14:55 +0200

    Implemented HTML decoder

--- src/medium_scraper.py
+++ src/medium_scraper.py
@@ -8,6 +8,7 @@ from typing import List
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
+import numpy as np
 import requests
 
 
@@ -15,7 +16,8 @@ class ErrorCodes(Enum):
     OK = 0
     MISSING_SCHEMA = 1
     CONNECTION_ERROR = 2
-    DECODING_ERROR = 3
+    NOT_FOUND = 3
+    DECODING_ERROR = 4
 
 
 @dataclass
@@ -37,69 +39,139 @@ class Article:
         }
 
 
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
-}
+class MediumScraper(object):
+    HEADERS = {
+        'accept': 'text/html,application/xhtml+xml,application/xml,application/json',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "x-xsrf-token": "1",
+    }
 
+    @classmethod
+    def scrape_medium_article(cls, url) -> Article:
+        url_parsed = urlparse(url)
 
-def scrape_medium_article(url) -> Article:
-    global HEADERS
+        try:
+            response = requests.get(f"{url}?format=json", cls.HEADERS)
+        except requests.exceptions.ConnectionError:
+            return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.CONNECTION_ERROR)
+        except requests.exceptions.MissingSchema:
+            return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.MISSING_SCHEMA)
 
-    url_parsed = urlparse(url)
+        if response.status_code == HTTPStatus.NOT_FOUND:
+            return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.NOT_FOUND)
 
-    try:
-        response = requests.get(f"{url}?format=json", HEADERS)
-    except requests.exceptions.ConnectionError:
-        return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.CONNECTION_ERROR)
-    except requests.exceptions.MissingSchema:
-        return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.MISSING_SCHEMA)
+        page_json = response.text[re.search(r"{", response.text).start():]
 
-    page_json = response.text[re.search(r"{", response.text).start():]
+        try:
+            page = json.loads(page_json)
+            title, author, paragraphs = cls.scrape_medium_article_in_json_format(page)
+        except JSONDecodeError:
+            title, author, paragraphs = cls.scrape_medium_article_in_html_format(response.text)
+        except Exception:
+            return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.DECODING_ERROR)
 
-    try:
-        page = json.loads(page_json)
-    except JSONDecodeError:
-        return Article(url=url_parsed.netloc, path=url_parsed.path, error_code=ErrorCodes.DECODING_ERROR)
+        return Article(
+            url=url_parsed.netloc,
+            path=url_parsed.path,
+            author=author,
+            title=title,
+            paragraphs=paragraphs
+        )
 
-    title, author, paragraphs = scrape_medium_article_in_json_format(page)
+    @staticmethod
+    def scrape_medium_article_in_json_format(page):
+        title = page["payload"]["value"]["title"]
+        creator_id = page["payload"]["value"]["creatorId"]
+        author = page["payload"]["references"]["User"][creator_id]["name"]
+        paragraphs = [
+            paragraph["text"]
+            for paragraph in page["payload"]["value"]["content"]["bodyModel"]["paragraphs"]
+            if paragraph["type"]
+        ]
 
-    return Article(
-        url=url_parsed.netloc,
-        path=url_parsed.path,
-        author=author,
-        title=title,
-        paragraphs=paragraphs
-    )
+        return title, author, paragraphs
 
+    @staticmethod
+    def scrape_medium_article_in_html_format(page_html):
+        page = BeautifulSoup(page_html, "html.parser")
+        title = page.find("meta", attrs={"name": "title"})["content"]
+        author = page.find("meta", attrs={"name": "author"})["content"]
+        paragraphs = [
+            "".join(element.find_all(text=True))
+            for element in page.find("article").find_all(["p", "li", "h1", "h2", "h3", "h4"])
+        ]
 
-def scrape_medium_article_in_json_format(page):
-    title = page["payload"]["value"]["title"]
-    creator_id = page["payload"]["value"]["creatorId"]
-    author = page["payload"]["references"]["User"][creator_id]["name"]
-    paragraphs = [
-        paragraph["text"]
-        for paragraph in page["payload"]["value"]["content"]["bodyModel"]["paragraphs"]
-        if paragraph["type"]
-    ]
+        return title, author, paragraphs
 
-    return title, author, paragraphs
+    @classmethod
+    def search_articles(cls, search_term: str, num_articles=10):
+        def get_article_id(url):
+            return url.split("-")[-1]
 
+        headers = cls.HEADERS.copy()
 
-def search_articles(search_term: str):
-    response = requests.get(f"https://medium.com/search?q={search_term}")
-    if response.status_code != HTTPStatus.OK:
-        raise RuntimeError(f"Search returned an HTTP status code {response.status_code}")
+        session = requests.session()
 
-    return get_article_urls(response.text)
+        base_url = f"https://medium.com/search/posts?q={search_term}"
 
+        urls = []
+        article_ids = []
+        article_ids_batch = None
+        num_batches = int(np.ceil(num_articles / 10))
+        for i in range(1, num_batches + 1):
+            print(f"Batch {i} of {num_batches}", end="\r")
+            if i == 1:
+                response = session.get(f"https://medium.com/search?q={search_term}", headers=headers)
+            elif i == 2:
+                ignore_query_parameters = "&".join([f"ignore={article_id}" for article_id in article_ids])
+                response = session.get(
+                    f"{base_url}&count=10&{ignore_query_parameters}",
+                    headers=headers
+                )
+            elif i == 3:
+                response = session.post(
+                    base_url,
+                    headers=headers,
+                    data={
+                        "ignoredIds": article_ids,
+                        "page": 3,
+                        "pageSize": 10
+                    }
+                )
+            else:
+                response = session.post(
+                    base_url,
+                    headers=headers,
+                    data={
+                        "ignoredIds": article_ids_batch,
+                        "page": i,
+                        "pageSize": 10
+                    }
+                )
 
-def get_article_urls(search_result: str):
-    def remove_query_parameters(url):
-        url_components = urlparse(url)
-        return f"{url_components.scheme}://{url_components.netloc}{url_components.path}"
+            if response.status_code != HTTPStatus.OK:
+                raise RuntimeError(f"Search returned an HTTP status code {response.status_code}")
 
-    search_result = BeautifulSoup(search_result, "html.parser")
-    article_list = search_result.find("div", attrs={"class": "js-postListHandle"})
-    articles = article_list.find_all("div", attrs={"class": "postArticle-content"})
+            headers['cookie'] = '; '.join([x.name + '=' + x.value for x in response.cookies])
 
-    return [remove_query_parameters(article.find("a")["href"]) for article in articles]
+            urls_batch = cls.get_article_urls(response.text)
+            article_ids_batch = [get_article_id(url) for url in urls_batch]
+
+            urls += urls_batch
+            article_ids += article_ids_batch
+
+        return urls, article_ids
+
+    @staticmethod
+    def get_article_urls(search_result: str):
+        def remove_query_parameters(url):
+            url_components = urlparse(url)
+            return f"{url_components.scheme}://{url_components.netloc}{url_components.path}"
+
+        search_result = BeautifulSoup(search_result, "html.parser")
+        article_list = search_result.find("div", attrs={"class": "js-postListHandle"})
+        articles = article_list.find_all("div", attrs={"class": "postArticle-content"})
+
+        return [remove_query_parameters(article.find("a")["href"]) for article in articles]
--- tests/test_medium_scrapper.py
+++ tests/test_medium_scrapper.py
@@ -4,7 +4,7 @@ from unittest import mock
 import requests
 import requests_mock
 
-from src.medium_scraper import ErrorCodes, scrape_medium_article, get_article_urls, search_articles
+from src.medium_scraper import ErrorCodes, MediumScraper
 
 
 class MediumScrapperTests(unittest.TestCase):
@@ -14,9 +14,19 @@ class MediumScrapperTests(unittest.TestCase):
         get_mock.side_effect = requests.exceptions.ConnectionError
         url = "http://WrongUrl.asdf"
         # When
-        article = scrape_medium_article(url)
+        article = MediumScraper.scrape_medium_article(url)
         # Then
         self.assertEqual(ErrorCodes.CONNECTION_ERROR, article.error_code)
+
+    @requests_mock.Mocker()
+    def test_not_found_page_returns_article_with_error_code(self, mock):
+        # Given
+        url = "http://not_found.com"
+        mock.get("http://not_found.com?format=json", status_code=404)
+        # When
+        article = MediumScraper.scrape_medium_article(url)
+        # Then
+        self.assertEqual(ErrorCodes.NOT_FOUND, article.error_code)
 
     @mock.patch("requests.get")
     def test_url_with_missing_schema_returns_article_with_error_code(self, get_mock):
@@ -24,43 +34,9 @@ class MediumScrapperTests(unittest.TestCase):
         get_mock.side_effect = requests.exceptions.MissingSchema
         url = "google.com"
         # When
-        article = scrape_medium_article(url)
+        article = MediumScraper.scrape_medium_article(url)
         # Then
         self.assertEqual(ErrorCodes.MISSING_SCHEMA, article.error_code)
-
-    @requests_mock.Mocker()
-    def test_return_decoding_error_when_json_decode_error_is_raised_because_returned_page_was_html_and_not_json(self, mock):
-        # Given
-        with open("tests/helpers/example_article_1.html", "r", encoding="utf-8") as file:
-            html = file.read()
-        mock.get("http://some_article.com?format=json", text=html)
-        url = "http://some_article.com"
-        # When
-        article = scrape_medium_article(url)
-        # Then
-        self.assertEqual(ErrorCodes.DECODING_ERROR, article.error_code)
-
-    @requests_mock.Mocker()
-    def test_find_medium_article_title(self, mock):
-        # Given
-        with open("tests/helpers/example_article_1.json", "r", encoding="utf-8") as file:
-            article_json = file.read()
-        mock.get("http://some_article.com?format=json", text=article_json)
-        # When
-        article = scrape_medium_article("http://some_article.com")
-        # Then
-        self.assertEqual("SEO Secrets: Reverse-Engineering Google’s Algorithm", article.title)
-
-    @requests_mock.Mocker()
-    def test_find_medium_article_author(self, mock):
-        # Given
-        with open("tests/helpers/example_article_1.json", "r", encoding="utf-8") as file:
-            article_json = file.read()
-        mock.get("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39?format=json", text=article_json)
-        # When
-        article = scrape_medium_article("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39")
-        # Then
-        self.assertEqual("benjamin bannister", article.author)
 
     @requests_mock.Mocker()
     def test_separate_url_and_path(self, mock):
@@ -69,19 +45,63 @@ class MediumScrapperTests(unittest.TestCase):
             article_json = file.read()
         mock.get("http://some_article.com/some_path/article?format=json", text=article_json)
         # When
-        article = scrape_medium_article("http://some_article.com/some_path/article")
+        article = MediumScraper.scrape_medium_article("http://some_article.com/some_path/article")
         # Then
         self.assertEqual("some_article.com", article.url)
         self.assertEqual("/some_path/article", article.path)
 
     @requests_mock.Mocker()
-    def test_get_article_body(self, mock):
+    def test_find_medium_article_title_when_returned_page_is_json(self, mock):
+        # Given
+        with open("tests/helpers/example_article_1.json", "r", encoding="utf-8") as file:
+            article_json = file.read()
+        mock.get("http://some_article.com?format=json", text=article_json)
+        # When
+        article = MediumScraper.scrape_medium_article("http://some_article.com")
+        # Then
+        self.assertEqual("SEO Secrets: Reverse-Engineering Google’s Algorithm", article.title)
+
+    @requests_mock.Mocker()
+    def test_find_medium_article_title_when_returned_page_is_html(self, mock):
+        # Given
+        with open("tests/helpers/example_article_1.html", "r", encoding="utf-8") as file:
+            article_html = file.read()
+        mock.get("http://some_article.com?format=json", text=article_html)
+        # When
+        article = MediumScraper.scrape_medium_article("http://some_article.com")
+        # Then
+        self.assertEqual("SEO Secrets: Reverse-Engineering Google’s Algorithm | by benjamin bannister | freeCodeCamp.org | Medium", article.title)
+
+    @requests_mock.Mocker()
+    def test_find_medium_article_author_when_returned_page_is_json(self, mock):
+        # Given
+        with open("tests/helpers/example_article_1.json", "r", encoding="utf-8") as file:
+            article_json = file.read()
+        mock.get("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39?format=json", text=article_json)
+        # When
+        article = MediumScraper.scrape_medium_article("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39")
+        # Then
+        self.assertEqual("benjamin bannister", article.author)
+
+    @requests_mock.Mocker()
+    def test_find_medium_article_author_when_returned_page_is_html(self, mock):
+        # Given
+        with open("tests/helpers/example_article_1.html", "r", encoding="utf-8") as file:
+            article_json = file.read()
+        mock.get("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39?format=json", text=article_json)
+        # When
+        article = MediumScraper.scrape_medium_article("https://medium.com/free-code-camp/seo-secrets-reverse-engineering-googles-algorithm-92fad4f5a39")
+        # Then
+        self.assertEqual("benjamin bannister", article.author)
+
+    @requests_mock.Mocker()
+    def test_get_article_body_when_returned_page_is_json(self, mock):
         # Given
         with open("tests/helpers/example_article_1.json", "r", encoding="utf-8") as file:
             article_json = file.read()
         mock.get("http://some_article.com/some_path/article?format=json", text=article_json)
         # When
-        article = scrape_medium_article("http://some_article.com/some_path/article")
+        article = MediumScraper.scrape_medium_article("http://some_article.com/some_path/article")
         # Then
         self.assertEqual("SEO Secrets: Reverse-Engineering Google’s Algorithm", article.paragraphs[0])
         self.assertEqual("SEO. Three letters to know if you want your content found. Image: benjamin bannister", article.paragraphs[1])
@@ -91,12 +111,28 @@ class MediumScrapperTests(unittest.TestCase):
             article.paragraphs[2]
         )
 
+    @requests_mock.Mocker()
+    def test_get_article_body_when_returned_page_is_html(self, mock):
+        # Given
+        with open("tests/helpers/example_article_1.html", "r", encoding="utf-8") as file:
+            article_json = file.read()
+        mock.get("http://some_article.com/some_path/article?format=json", text=article_json)
+        # When
+        article = MediumScraper.scrape_medium_article("http://some_article.com/some_path/article")
+        # Then
+        self.assertEqual("SEO Secrets: Reverse-Engineering Google’s Algorithm", article.paragraphs[0])
+        self.assertEqual(
+            "What have I learned from creating content for the internet? One thing is crystal clear:"
+            " if you want people to discover your work, you need search engine optimization (SEO).",
+            article.paragraphs[1]
+        )
+
     def test_get_urls_from_search_results(self):
         # Given
         with open("tests/helpers/example_search_result_1.html", "r", encoding="utf-8") as file:
             search_result = file.read()
         # When
-        urls = get_article_urls(search_result)
+        urls = MediumScraper.get_article_urls(search_result)
         # Then
         self.assertEqual(
             [
@@ -120,7 +156,7 @@ class MediumScrapperTests(unittest.TestCase):
             search_result = file.read()
         mock.get("https://medium.com/search?q=mySearchTerm", text=search_result)
         # When
-        urls = search_articles("mySearchTerm")
+        urls, article_ids = MediumScraper.search_articles("mySearchTerm", num_articles=10)
         # Then
         self.assertEqual(1, mock.call_count)
         self.assertEqual("https://medium.com/search?q=mySearchTerm", mock.request_history[0].url)
@@ -139,3 +175,19 @@ class MediumScrapperTests(unittest.TestCase):
             ],
             urls
         )
+        self.assertEqual(
+            [
+                "b64136629760",
+                "9249f18e8e76",
+                "b21bfc1f63c7",
+                "d534b69ce1c7",
+                "97153741cfe6",
+                "765fe895d2d3",
+                "7777e6d0441f",
+                "d36b8a1d000d",
+                "285f75337702",
+                "a7c111671338",
+            ],
+            article_ids
+        )
+
